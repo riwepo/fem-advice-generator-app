624a106776f1c554f526de1db473c5cdb270e060
Richard Potter <richardpotter.au@gmail.com>
2023-07-25 10:41:31 +1000

    use advice component

--- src/Components/Advice.js
+++ src/Components/Advice.js
@@ -1,9 +1,32 @@
 import React from "react";
 
+import patternDividerMobile from "../images/pattern-divider-mobile.svg";
+import patternDividerDesktop from "../images/pattern-divider-desktop.svg";
+import iconDice from "../images/icon-dice.svg";
+
 import classes from "./Advice.module.css";
 
 function Advice() {
-  return <h1 className={classes.header}>advice</h1>;
+  return (
+    <main className={classes.main}>
+      <h1 className={classes.header}>Advice #35</h1>
+      <p className={classes.advice}>
+        It is easy to sit up and take notice. What is difficult is to get up and
+        take action.
+      </p>
+      <picture>
+        <source media="(min-width: 768px)" srcSet={patternDividerDesktop} />
+        <img
+          className={classes.imgDivider}
+          src={patternDividerMobile}
+          alt="young man holding laptop"
+        />
+      </picture>
+      <button className={classes.button}>
+        <img className={classes.imgButton} src={iconDice} alt="dice icon" />
+      </button>
+    </main>
+  );
 }
 
 export default Advice;
